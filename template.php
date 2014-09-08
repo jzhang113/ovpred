09b27d3b2516bb8ec87c527ca5e505e9842c0892
Brandon Neil <brandon-neil@uiowa.edu>
2014-09-08 13:48:49 -0500

    Fixed template preprocess issue

--- template.php
+++ template.php
@@ -35,12 +35,3 @@ function ovpred_breadcrumb(&$variables) {
     return $output;
   }
 }
-
-/**
- * Implements theme_preprocess_node().
- */
-function ovpred_preprocess_node(&$variables) {
-  if($variables['type'] == 'featured_content') {
-    $variables['feature_link'] = $variables['field_feature_link'][LANGUAGE_NONE][0]['url'];
-  }
-}
