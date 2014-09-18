a986311c7d2f07377047bc22e2cd8d1c004a8732
Brandon Neil <brandon-neil@uiowa.edu>
2014-09-18 13:56:23 -0500

    Cleaned up date printing

--- templates/node--article.tpl.php
+++ templates/node--article.tpl.php
@@ -101,7 +101,9 @@
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
-      print $date;
+      if($page) {
+        print $date;
+      }
       print render($content);
     ?>
   </div>
